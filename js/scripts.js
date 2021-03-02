c219c723bedfdaed164c001ad7066af9d8f8c4e1
fatma khamis <fatmaali#hotmail.com>
2021-03-02 17:50:39 +0300

    add script file and link to index file

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 /*$(document).ready(function()*/
 $("#clickable1").click(function() {
-  $(".hide1").fade();
-  $(".show1").fade();
+  $(".hide1").fadeToggle(3000);
+  $(".show1").fadeToggle(3000);
 });
 
 $("#clickable2").click(function() {
-  $(".hide2").fade();
-  $(".show2").fade();
+  $(".hide2").fadeToggle(3000);
+  $(".show2").fadeToggle(3000);
 });
 $("#clickable3").click(function() {
-  $(".hide3").fade();
-  $(".show3").fade();
+  $(".hide3").fadeToggle(3000);
+  $(".show3").fadeToggle(3000);
 });
 /**$("#sub").click(function(event) {
   var name = $("#name").val();
