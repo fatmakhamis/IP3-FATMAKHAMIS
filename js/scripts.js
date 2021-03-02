13704328a2fdbcfaf5be4d43a48ac588c6cfbe5f
fatma khamis <fatmaali#hotmail.com>
2021-03-03 00:49:01 +0300

    add toffle in idex file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*$(document).ready(function()*/
-$("#clickable1").click(function() {
-  $(".hide1").fadeToggle(3000);
-  $(".show1").fadeToggle(3000);
+/* $("#clickable1").click(function() {
+  $(".hide1").removeClassName();
+  $(".show1").addClassName(hide1);
 });
 
 $("#clickable2").click(function() {
@@ -17,3 +17,37 @@ $("#clickable3").click(function() {
   alert(name + " Thank you for replying");
   event.preventDefault();
 }**/
+
+
+            
+            
+             $("#clickable1").on('click',function(){
+            $("#clickable1").fadeToggle(2000 ,function(){;
+              $("#hide2").hide();
+              $("#hide3").hide();
+              $("#clickable2").show();
+              $("#clickable3").show();
+            $("#hide1").show();
+            });
+            });
+           
+            $("#clickable2").on('click',function(){
+            $("#clickable2").fadeToggle(2000 ,function(){;
+            $("#hide1").hide();
+            $("#clickable1").show();
+            $("#clickable3").show();
+            $("#hide3").hide();
+            $("#hide2").show();
+            });
+            });
+
+            $("#clickable3").on('click',function(){
+            $("#clickable3").fadeToggle(2000 ,function(){;
+            $("#hide2").hide();
+            $("#hide1").hide();
+            $("#clickable2").show();
+            $("#clickable1").show();
+            $("#hide3").show();
+            });
+            });
+              
