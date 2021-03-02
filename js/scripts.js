cb0bd84c76a5de590d1ac254d3b0dbe8a3a909c9
fatma khamis <fatmaali#hotmail.com>
2021-03-02 17:24:35 +0300

    add script file and link to index file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
-$(document).ready(function() {
-    $(".clickable").click(function() {
-      $(".walrus-showing").fadeToggle();
-      $(".walrus-hidden").fadeToggle();
-    });
-  });
-  
+/*$(document).ready(function()*/
+$("#clickable1").click(function() {
+  $(".hide1").fade();
+  $(".show1").fade();
+});
+
+$("#clickable2").click(function() {
+  $(".hide2").fade();
+  $(".show2").fade();
+});
+$("#clickable3").click(function() {
+  $(".hide3").fade();
+  $(".show3").fade();
+});
+/**$("#sub").click(function(event) {
+  var name = $("#name").val();
+  alert(name + " Thank you for replying");
+  event.preventDefault();
+}**/
